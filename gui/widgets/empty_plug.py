7ceda3cafdafee58247c0a74a09fff1cb734dc86
QkForce <nurbergen.kuanysh@gmail.com>
2026-03-21 21:02:27 +0500

    refactor(gui): remove unnecessary instance variables from EmptyPlug

--- gui/widgets/empty_plug.py
+++ gui/widgets/empty_plug.py
@@ -9,8 +9,6 @@ from gui.constants.colors import AppColors
 class EmptyPlug(QWidget):
     def __init__(self, title, description):
         super().__init__()
-        self.title = title
-        self.description = description
 
         icon_lbl = QLabel()
         icon_pixmap = get_svg_pixmap(
@@ -25,20 +23,19 @@ class EmptyPlug(QWidget):
         icon_frame.setProperty("frame-style", "status-empty")
         icon_frame.setLayout(icon_layout)
 
-        title_lbl = QLabel(self.title)
+        title_lbl = QLabel(title)
         title_lbl.setProperty("lbl-level", "h2")
         # title_lbl.setWordWrap(True)
 
-        description_lbl = QLabel(self.description)
+        description_lbl = QLabel(description)
         description_lbl.setProperty("lbl-level", "h3-normal")
         # description_lbl.setWordWrap(True)
 
         layout = QVBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
         layout.addStretch()
         layout.addWidget(icon_frame, alignment=Qt.AlignCenter)
         layout.addSpacing(20)
         layout.addWidget(title_lbl, alignment=Qt.AlignCenter)
         layout.addWidget(description_lbl, alignment=Qt.AlignCenter)
         layout.addStretch()
-
-        self.setContentsMargins(0, 0, 0, 0)
